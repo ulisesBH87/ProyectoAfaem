19ccddd9febb9a18ee6c76c1a7ef24ac4619c483
ChristianBZ360 <20680051@cuautla.tecnm.mx>
2026-03-03 12:19:40 -0600

    feat(tiposAfiliacion): Agregado el catalogo
    Se agregó el catalogo de tipos de afiliación y el seed para insertar los tipos automáticamente en la bdd

--- backend/app/modelos/__init__.py
+++ backend/app/modelos/__init__.py
@@ -3,3 +3,4 @@ from .rol_modelo import Rol
 from .sexo_c_modelo import CatalogoSexo
 from .solicitud_modelo import Solicitud
 from .catalogo_estado_validacion import CatalogoEstadosValidacion
+from .catalogo_tipo_afiliacion import CatalogoTiposAfiliacion
