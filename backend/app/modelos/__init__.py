16b32e295bfdba400e445c5f12acade8d784586c
ChristianBZ360 <20680051@cuautla.tecnm.mx>
2026-03-03 16:15:33 -0600

    feat(db): Add catalogo de documentos

--- backend/app/modelos/__init__.py
+++ backend/app/modelos/__init__.py
@@ -5,3 +5,4 @@ from .solicitud_modelo import Solicitud
 from .catalogo_estado_validacion import CatalogoEstadosValidacion
 from .catalogo_tipo_afiliacion import CatalogoTiposAfiliacion
 from .catalogo_tipos_archivo import CatalogoTiposArchivo
+from .catalogo_documento import CatalogoDocumentos
--- backend/app/modelos/catalogo_documento.py
+++ backend/app/modelos/catalogo_documento.py
@@ -1,13 +1,11 @@
-
-"""from sqlalchemy import Column, Integer, String, CHAR, Date, Boolean, ForeignKey, CheckConstraint
+from sqlalchemy import Column, Integer, String, CHAR, Date, Boolean, ForeignKey, CheckConstraint
 from sqlalchemy.orm import relationship
 from app.db.base import Base
-
-
 
 class CatalogoDocumentos(Base):
     __tablename__ = "CatalogoDocumentos"
     DocumentoId = Column(Integer, primary_key=True)
-    Nombre = Column(String(100), nullable=False, unique=True)
+    NombreDocumento = Column(String(100), nullable=False, unique=True)
+    Descripcion = Column(String(200), nullable=True)
 
-"""
+    TipoArchivoId = Column(Integer, ForeignKey("CatalogoTiposArchivo.TipoArchivoId"), nullable=True)
