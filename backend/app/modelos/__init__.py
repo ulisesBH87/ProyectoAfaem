d016054027ef3c80c49bcc37108eb033eb0846aa
ChristianBZ360 <20680051@cuautla.tecnm.mx>
2026-03-03 13:37:40 -0600

    feat(db): Add catálogo tipos de archivo
    - Creación del catálogo
    - Llenado de datos en el catálogo

--- backend/app/modelos/__init__.py
+++ backend/app/modelos/__init__.py
@@ -4,3 +4,4 @@ from .sexo_c_modelo import CatalogoSexo
 from .solicitud_modelo import Solicitud
 from .catalogo_estado_validacion import CatalogoEstadosValidacion
 from .catalogo_tipo_afiliacion import CatalogoTiposAfiliacion
+from .catalogo_tipos_archivo import CatalogoTiposArchivo
