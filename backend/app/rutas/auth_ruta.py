725af2ff7e807d5426358853dad6d529677b876d
ChristianBZ360 <20680051@cuautla.tecnm.mx>
2026-03-11 15:58:02 -0600

    feat(roles): enviar rol en el token

--- backend/app/rutas/auth_ruta.py
+++ backend/app/rutas/auth_ruta.py
@@ -44,6 +44,11 @@ def login(data: InicioSesion, db:Session = Depends(get_db)) -> TokenResponse:
     return {
         "access_token": token_generado,
         "token_type": "bearer"
+        "usuario": {
+            "id": usuarioIntentoSesion.UsuarioId,
+            "correo": usuarioIntentoSesion.correo,
+            "rol": usuarioIntentoSesion.rol.nombre
+        }
     }
 
 @router.post("/cambiar-contrasena")
