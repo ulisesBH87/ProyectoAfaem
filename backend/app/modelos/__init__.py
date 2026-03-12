ce974e5255f617094bf26f05d116cf8516f3b870
ChristianBZ360 <20680051@cuautla.tecnm.mx>
2026-03-12 12:08:30 -0600

    feat(db): Catalogo de seguros

--- backend/app/modelos/__init__.py
+++ backend/app/modelos/__init__.py
@@ -11,5 +11,6 @@ from .catalogo_rol_personas import CatalogoRolesPersonas
 from .documento_afiliacion_modelo import DocumentoAfiliacion
 from .documentos_entregados_modelo import DocumentosEntregados
 from .catalogo_documentos_persona import CatalogoDocumentosPersonas
-
 from .roles_modelo import Roles
+
+from .catalogo_seguros import Seguro
