f79a32b8893184c0d2afa59091930be1a7af4aef
Christian <20680051@cuautla.tecnm.mx>
2026-03-20 14:09:20 -0600

    feat(db): Catalogo tipos de proceso de equipo temporal

--- backend/app/modelos/__init__.py
+++ backend/app/modelos/__init__.py
@@ -18,5 +18,6 @@ from .catalogo_estatus_pago import EstatusPago
 from .ordenes_pago_modelo import OrdenPago
 from .orden_pago_detalle_modelo import OrdenPagoDetalle
 from .catalogo_estatus_presidente import EstatusPresidente
-
 from .presidente_equipo_modelo import PresidenteEquipo
+
+from .catalogo_tipo_proceso import CatalogoTipoProceso
---
+++ backend/app/modelos/catalogo_tipo_proceso.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, CHAR, Date, DateTime, Boolean, ForeignKey, CheckConstraint
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+class CatalogoTipoProceso(Base):
+    __tablename__ = "CatalogoTipoProceso"
+
+    TipoProceso = Column(Integer, primary_key="True")
+    Nombre = Column(String)
