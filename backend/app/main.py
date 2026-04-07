8e9efd21abbfe531c44b1bca2ab2195ca3c1eaed
Christian Bazan <chrisbazan360@gmail.com>
2026-04-07 10:10:59 -0600

    cleanup: eliminación de redundancia en CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,13 +13,12 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
         "http://192.168.0.172:3000",
+
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
         "http://192.168.0.172:5173",
+        
         "http://localhost:5174",
-        "http://127.0.0.1:5174",
         "http://192.168.0.172:5174",
     ],
     allow_credentials=True,
