f96740fc250ed87c7ff8c462cddf042774281d87
Emiliano Saaib <21680072@cuautla.tecnm.mx>
2026-04-17 09:52:50 -0600

    style(ui): reemplazar iconos de futbol americano por soccer globalmente

--- Front_Feo/AFAEM-Front/src/components/DashboardSidebar.jsx
+++ Front_Feo/AFAEM-Front/src/components/DashboardSidebar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import {
-  FaFootballBall,
   FaSignOutAlt,
   FaChevronLeft,
   FaChevronRight
