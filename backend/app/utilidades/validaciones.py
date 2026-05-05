28766c51d1978e9fdd4e278cc3436f853b27e5b4
Christian Bazan <chrisbazan360@gmail.com>
2026-05-05 13:18:18 -0600

    feat: validación de curp backend

---
+++ backend/app/utilidades/validaciones.py
@@ -0,0 +1,19 @@
+
+from sympy import re
+
+
+def validacion_curp(value: str):
+        val = value.upper()
+        curp_regex = (
+            r'^([A-Z][AEIOUX][A-Z]{2}\d{2}' # Letras iniciales y año
+            r'(?:0[1-9]|1[0-2])'            # Mes (01-12)
+            r'(?:0[1-9]|[12]\d|3[01])'      # Día (01-31)
+            r'[HM]'                         # Género
+            r'(?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)' # Estados
+            r'[B-DF-HJ-NP-TV-Z]{3}'         # Consonantes internas
+            r'[A-Z\d])(\d)$'                # Homoclave y dígito verificador
+        )
+
+        if not re.fullmatch(curp_regex, val):
+            raise ValueError('Formato de CURP inválido')
+        return val
