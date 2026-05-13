6a2d230f0f9afbb81ffcdfd11a7770a0a43e6388
JesusPerez <yesper233@gmail.com>
2026-05-13 14:06:52 -0600

    Fix(endpoint) Catalogos
    Con este cambio se espera que el panel catalogos use el endpoint "/equipo-temporal/catalogos-registro" Y muestre correctamente la informacion en el front, listo para madnar el dist

--- backend/app/main.py
+++ backend/app/main.py
@@ -158,7 +158,9 @@ app.add_middleware(
         "http://192.168.0.172:5173",
         "http://localhost:5174",
         "http://192.168.0.172:5174",
-        # Producción
+        # Producción - Servidor IIS
+        "http://201.131.21.213",
+        "http://201.131.21.213:80",
         "http://afaem.scholatek.com",
         "https://afaem.scholatek.com",
     ],
