4ec61dc5df179481209f5dda851cf0a3fe43bc7b
JesusPerez <yesper233@gmail.com>
2026-05-14 10:57:49 -0600

    configuración de conexión con servidor (Backend).

--- Front_Feo/AFAEM-Front/src/config/config.js
+++ Front_Feo/AFAEM-Front/src/config/config.js
@@ -1,3 +1,4 @@
-export const API_BASE = ''; // MULTI-PROXY SUPPORT (VITE)
+//export const API_BASE = ''; // LOCAL - Descomentar para desarrollo local con Vite proxy
+export const API_BASE = '/afaem/api'; // PRODUCCIÓN IIS - Comentar para desarrollo local
 export const API_CANDIDATES = [API_BASE];
 export default { API_BASE, API_CANDIDATES };
