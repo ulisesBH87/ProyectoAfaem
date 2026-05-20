890322ab5594c81a286cb00473f128068d3d3db6
Christian Bazan <chrisbazan360@gmail.com>
2026-05-20 10:17:21 -0600

    feat: separacion de seguros de presidente y seguros de jugador

--- backend/app/modelos/catalogo_seguros.py
+++ backend/app/modelos/catalogo_seguros.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DECIMAL, Boolean
+from sqlalchemy import Column, Integer, String, DECIMAL, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from app.db.base import Base
 
@@ -11,5 +11,5 @@ class Seguro(Base):
 
     OrdenPagoDetalleRelacion = relationship("OrdenPagoDetalle", back_populates="SeguroRelacion")
     EquipoTemporalJugadorRelacion= relationship("EquipoTemporalJugador", back_populates="SeguroRelacion")
-    
-    
+
+    TipoPersonaId = Column(Integer, ForeignKey("CatalogoRolesPersonas.RolPersonaId"), nullable=True)
