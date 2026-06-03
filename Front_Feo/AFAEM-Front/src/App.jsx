b0b28483d799ec27df12e9eaf066b4c21a2bb577
Christian Bazan <chrisbazan360@gmail.com>
2026-06-03 09:38:23 -0600

    feat: ocultar nombre de ruta
    - Se cambió el nombre de ruta "invitacion" por "i" para formulario de registro de jugadores

--- Front_Feo/AFAEM-Front/src/App.jsx
+++ Front_Feo/AFAEM-Front/src/App.jsx
@@ -126,7 +126,7 @@ function App() {
           <Route path="/olvide-contrasena" element={<OlvideContrasena />} />
           <Route path="/restablecer-contrasena" element={<RestablecerContrasena />} />
           <Route path="/suspendido" element={<Suspended />} />
-          <Route path="/invitacion/:token" element={<RegistroJugadores />} />
+          <Route path="/i/:token" element={<RegistroJugadores />} />
 
           {/* DASHBOARD ROUTES WRAPPED IN MAINLAYOUT AND GENERALGUARD */}
           <Route element={<GeneralGuard><MainLayout userEmail={userEmail} /></GeneralGuard>}>
