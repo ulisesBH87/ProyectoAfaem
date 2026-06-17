a8b5bc8e87cc5b01e897b7bd1cda4e096d088044
Christian Bazan <chrisbazan360@gmail.com>
2026-06-17 15:54:25 -0600

    feat: documentos pendientes

--- backend/app/enums/documentos_estatus_enum.py
+++ backend/app/enums/documentos_estatus_enum.py
@@ -2,13 +2,10 @@ from enum import Enum
 
 #Estatus de documentos
 class DocumentoEstatus(str, Enum):
-    
     #BUENOS, PONER DESPUÉS
-    #ESPERA = 1
-    #ACEPTADO = 2
-    #RECHAZADO = 3
-    #BORRADOR = 4
-    APROBADO = 1
-    PENDIENTE = 2
-    RECHAZADO = 3
-    BORRADOR = 4
+    ESPERA = "1"
+    PENDIENTE = "1"
+    ACEPTADO = "2"
+    APROBADO = "2"
+    RECHAZADO = "3"
+    BORRADOR = "4"
