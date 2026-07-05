860a4c2b8fb46769831001b69004af29e714afef
Christian Bazan <chrisbazan360@gmail.com>
2026-07-05 13:18:20 -0600

    refactor: loader azul

--- Front_Feo/AFAEM-Front/src/components/Loader.jsx
+++ Front_Feo/AFAEM-Front/src/components/Loader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/afaem-logo@4x1.png';
+import logo from '../assets/afaem-logo@4x1Azul.png';
 
 const Loader = ({ text = "AFAEM DIGITAL", inline = false }) => {
   return (
